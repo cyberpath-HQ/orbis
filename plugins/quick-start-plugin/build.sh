ea7972fe708562364cdf0329727daec0877cd085
ebalo55 <12586380+ebalo55@users.noreply.github.com>
2025-12-23 04:37:11 +0100

    chore(build): add error handling and strict mode to build script

--- plugins/quick-start-plugin/build.sh
+++ plugins/quick-start-plugin/build.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -euo pipefail
+
 # cargo build --target wasm32-unknown-unknown --release -p quick-start-plugin
 cargo build --target wasm32-unknown-unknown -p quick-start-plugin
 # cp ../../target/wasm32-unknown-unknown/release/quick_start_plugin.wasm ./quick_start_plugin.wasm
